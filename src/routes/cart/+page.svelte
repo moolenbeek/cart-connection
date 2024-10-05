b9a67a86b635804ac08520758af263f7b4115995
moolenbeek <emoolenbeek@gmail.com>
2024-10-04 20:35:15 -0400

    create component

---
+++ src/routes/cart/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import * as Card from "$lib/components/ui/card/index";
+</script>
+
+<div class="flex items-center justify-center pt-16">
+    <Card.Root class="w-[350px]">
+        <Card.Header>
+            <Card.Title>Cart</Card.Title>
+        </Card.Header>
+    </Card.Root>
+</div>
