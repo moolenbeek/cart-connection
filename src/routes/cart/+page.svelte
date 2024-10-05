dd338245df085b92b24045cacb1c4c0bedb48772
moolenbeek <emoolenbeek@gmail.com>
2024-10-04 20:39:00 -0400

    modify cart

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -6,6 +6,13 @@
     <Card.Root class="w-[350px]">
         <Card.Header>
             <Card.Title>Cart</Card.Title>
+            <Card.Description>Your cart is empty</Card.Description>
         </Card.Header>
+        <Card.Content>
+
+        </Card.Content>
+        <Card.Footer>
+
+        </Card.Footer>
     </Card.Root>
 </div>
