e7ee90cbea9c76747eaf6cb6a782ca7ec5a14ac5
moolenbeek <emoolenbeek@gmail.com>
2024-10-04 20:27:56 -0400

    create config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,14 @@
-import { config } from "dotenv";
-import { defineConfig } from "drizzle-kit";
-config({ path: ".env.local" });
-export default defineConfig({
-	schema: "./src/db/schema.ts",
-	out: "./migrations",
-	dialect: "postgresql",
-	dbCredentials: {
-		url: process.env.POSTGRES_URL!
-	}
-});
+import type { Config } from "drizzle-kit";
+const { POSTGRES_URL } = process.env;
+if (!POSTGRES_URL) {
+  throw new Error("No url");
+}
+export default {
+  schema: "./src/db/schema.ts",
+  breakpoints: true,
+  out: "./migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+	  url: process.env.POSTGRES_URL!
+  }
+} satisfies Config;
