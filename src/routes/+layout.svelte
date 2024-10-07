74c399c36f50491614368e5a2e42f8ddf2aa45c5
moolenbeek <emoolenbeek@gmail.com>
2024-10-07 07:55:50 -0400

    add vercel speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import "../app.css";
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import { ModeWatcher } from "mode-watcher";
 	import Navigation from "$lib/components/navigation/navigation.svelte";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
+	injectSpeedInsights();
 </script>
 
 <ModeWatcher />
-
 <Navigation user={data.user} />
 
 <slot />
